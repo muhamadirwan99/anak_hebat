154eabd5038183580c8533d970f006ebd17e6bd1
Muhamad Irwan <muhamadirwan99@gmail.com>
2025-08-08 20:22:47 +0700

    fix(router): update default state for materi route to ayoBelajar

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -24,8 +24,7 @@ final GoRouter newRouter = GoRouter(
       path: RouterUtils.materi,
       builder: (BuildContext context, GoRouterState state) {
         final EnumMateriState pageState =
-            state.extra as EnumMateriState? ?? EnumMateriState.mengenalPosisi;
-        // state.extra as EnumMateriState? ?? EnumMateriState.ayoBelajar;
+            state.extra as EnumMateriState? ?? EnumMateriState.ayoBelajar;
 
         return MateriView(
           pageState: pageState,
